efb93665d67ff517c504636a12f500873bfd38ab
ChengFu,Lin <chengfulin0806@gmail.com>
2018-09-04 18:28:55 +0800

    Modify cases

--- WebApplication3/ReportDemo.cs
+++ WebApplication3/ReportDemo.cs
@@ -1,6 +1,7 @@
 namespace WebApplication3
 {
     using System;
+    using System.Collections.Generic;
     using System.ComponentModel;
     using System.Drawing;
     using System.Windows.Forms;
@@ -19,129 +20,252 @@ namespace WebApplication3
             //
             InitializeComponent();
 
-            //
-            // TODO: Add any constructor code after InitializeComponent call
-            //
-            TryAddOneMoreRow();
-            TryAddOneMoreColumn();
+            // Uncomment one of the cases to see the result
+            //CreateTable2x2Test(); // case 1
+            //CreateTable2x2MergeHeaderTest(); // case 2
+            //CreateTable2x2MergeBodyCellTest(); // case 3
         }
 
-        private void TryAddOneMoreRow()
+        /// case 1:
+        /// | 0.0 | 0.1 |
+        /// -------------
+        /// | 1.0 | 1.1 |
+        /// 
+        private void CreateTable2x2Test()
         {
-            Telerik.Reporting.TableGroup tableGroup13 = new Telerik.Reporting.TableGroup();
-            var txtBox60 = new Telerik.Reporting.TextBox();
-            var txtBox62 = new Telerik.Reporting.TextBox();
-            var txtBox63 = new Telerik.Reporting.TextBox();
+            var table1 = new Telerik.Reporting.Table();
+            var colGroup1 = new Telerik.Reporting.TableGroup();
+            var colGroup2 = new Telerik.Reporting.TableGroup();
+            var colGroup3 = new Telerik.Reporting.TableGroup();
+            var colGroup4 = new Telerik.Reporting.TableGroup();
+            var detailGroup = new Telerik.Reporting.TableGroup();
+            var rowGroup1 = new Telerik.Reporting.TableGroup();
 
-            this.table1.Body.Rows.Add(new Telerik.Reporting.TableBodyRow(Telerik.Reporting.Drawing.Unit.Cm(0.5D)));
-            this.table1.Body.SetCellContent(5, 0, txtBox60, 1, 2);
-            this.table1.Body.SetCellContent(5, 2, txtBox62);
-            this.table1.Body.SetCellContent(5, 3, txtBox63);
-            this.table1.Items.AddRange(new Telerik.Reporting.ReportItemBase[] { txtBox60, txtBox62, txtBox63 });
+            var txtBox00 = new Telerik.Reporting.TextBox();
+            var txtBox01 = new Telerik.Reporting.TextBox();
+            var txtBox10 = new Telerik.Reporting.TextBox();
+            var txtBox11 = new Telerik.Reporting.TextBox();
 
-            tableGroup13.Name = "rowgroup_60";
-            this.table1.RowGroups[0].ChildGroups.Add(tableGroup13); // Get "detailTableGroup"
-            //
-            // txtBox60
-            // 
-            txtBox60.Name = "txtBox60";
-            txtBox60.Size = new Telerik.Reporting.Drawing.SizeU(Telerik.Reporting.Drawing.Unit.Cm(2.98979115486145D), Telerik.Reporting.Drawing.Unit.Cm(0.5D));
-            txtBox60.StyleName = "";
-            txtBox60.Value = "6.0";
-            //
-            // txtBox62
-            // 
-            txtBox62.Name = "txtBox62";
-            txtBox62.Size = new Telerik.Reporting.Drawing.SizeU(Telerik.Reporting.Drawing.Unit.Cm(2.98979115486145D), Telerik.Reporting.Drawing.Unit.Cm(0.5D));
-            txtBox62.StyleName = "";
-            txtBox62.Value = "6.2";
-            //
-            // txtBox63
-            // 
-            txtBox63.Name = "txtBox63";
-            txtBox63.Size = new Telerik.Reporting.Drawing.SizeU(Telerik.Reporting.Drawing.Unit.Cm(2.98979115486145D), Telerik.Reporting.Drawing.Unit.Cm(0.5D));
-            txtBox63.StyleName = "";
-            txtBox63.Value = "6.3";
-        }
+            this.detail.Items.AddRange(new Telerik.Reporting.ReportItemBase[] {
+            table1});
 
-        private void TryAddOneMoreColumn()
-        {
-            Telerik.Reporting.TableGroup colGroup04 = new Telerik.Reporting.TableGroup();
+            table1.Body.Columns.Add(new Telerik.Reporting.TableBodyColumn(Telerik.Reporting.Drawing.Unit.Cm(3D)));
+            table1.Body.Columns.Add(new Telerik.Reporting.TableBodyColumn(Telerik.Reporting.Drawing.Unit.Cm(3D)));
+            table1.Body.Rows.Add(new Telerik.Reporting.TableBodyRow(Telerik.Reporting.Drawing.Unit.Cm(0.5D)));
 
-            var txtBox04 = new Telerik.Reporting.TextBox();
-            var txtBox14 = new Telerik.Reporting.TextBox();
-            var txtBox24 = new Telerik.Reporting.TextBox();
-            var txtBox34 = new Telerik.Reporting.TextBox();
-            var txtBox44 = new Telerik.Reporting.TextBox();
-            var txtBox54 = new Telerik.Reporting.TextBox();
-            var txtBox64 = new Telerik.Reporting.TextBox();
+            table1.Body.SetCellContent(0, 0, txtBox10);
+            table1.Body.SetCellContent(0, 1, txtBox11);
 
-            this.table1.Body.Columns.Add(new Telerik.Reporting.TableBodyColumn(Telerik.Reporting.Drawing.Unit.Cm(2D)));
-
-            this.table1.Body.SetCellContent(0, 4, txtBox14);
-            this.table1.Body.SetCellContent(1, 4, txtBox24);
-            this.table1.Body.SetCellContent(2, 4, txtBox34);
-            this.table1.Body.SetCellContent(3, 4, txtBox44);
-            this.table1.Body.SetCellContent(4, 4, txtBox54);
-            this.table1.Body.SetCellContent(5, 4, txtBox64);
-            
-            colGroup04.Name = "colgroup_04";
-            colGroup04.ReportItem = txtBox04;
-            this.table1.ColumnGroups.Add(colGroup04);
-
-            this.table1.Items.AddRange(new Telerik.Reporting.ReportItemBase[] {
-                txtBox04, txtBox14, txtBox24, txtBox34, txtBox44, txtBox54, txtBox64
+            table1.Items.AddRange(new Telerik.Reporting.ReportItemBase[] {
+                txtBox00, txtBox01, txtBox10, txtBox11
             });
 
+            colGroup2.Name = "colgroup-02";
+            colGroup1.ChildGroups.Add(colGroup2);
+            colGroup1.Name = "colgroup-01";
+            colGroup1.ReportItem = txtBox00;
+
+            colGroup4.Name = "colgroup-04";
+            colGroup3.ChildGroups.Add(colGroup4);
+            colGroup3.Name = "colgroup-03";
+            colGroup3.ReportItem = txtBox01;
+
+            table1.ColumnGroups.Add(colGroup1);
+            table1.ColumnGroups.Add(colGroup3);
+
+            rowGroup1.Name = "rowgroup-01";
+            detailGroup.ChildGroups.Add(rowGroup1);
+            detailGroup.Groupings.Add(new Telerik.Reporting.Grouping(null));
+            detailGroup.Name = "detailgroup";
+
+            table1.RowGroups.Add(detailGroup);
+
+            table1.Size = new Telerik.Reporting.Drawing.SizeU(Telerik.Reporting.Drawing.Unit.Cm(6D), Telerik.Reporting.Drawing.Unit.Cm(1D));
+            table1.Location = new Telerik.Reporting.Drawing.PointU(Telerik.Reporting.Drawing.Unit.Cm(1.5D), Telerik.Reporting.Drawing.Unit.Cm(1.5D));
+            table1.Name = "table1";
+
             //
-            // txtBox04
-            // 
-            txtBox04.Name = "txtBox04";
-            txtBox04.Size = new Telerik.Reporting.Drawing.SizeU(Telerik.Reporting.Drawing.Unit.Cm(2.98979115486145D), Telerik.Reporting.Drawing.Unit.Cm(0.5D));
-            txtBox04.StyleName = "";
-            txtBox04.Value = "0.4";
+            // txtBox00
             //
-            // txtBox14
-            // 
-            txtBox14.Name = "txtBox14";
-            txtBox14.Size = new Telerik.Reporting.Drawing.SizeU(Telerik.Reporting.Drawing.Unit.Cm(2.98979115486145D), Telerik.Reporting.Drawing.Unit.Cm(0.5D));
-            txtBox14.StyleName = "";
-            txtBox14.Value = "1.4";
+            txtBox00.Name = "txtBox00";
+            txtBox00.Size = new Telerik.Reporting.Drawing.SizeU(Telerik.Reporting.Drawing.Unit.Cm(3D), Telerik.Reporting.Drawing.Unit.Cm(0.5D));
+            txtBox00.StyleName = "";
+            txtBox00.Value = "0.0";
             //
-            // txtBox24
-            // 
-            txtBox24.Name = "txtBox24";
-            txtBox24.Size = new Telerik.Reporting.Drawing.SizeU(Telerik.Reporting.Drawing.Unit.Cm(2.98979115486145D), Telerik.Reporting.Drawing.Unit.Cm(0.5D));
-            txtBox24.StyleName = "";
-            txtBox24.Value = "2.4";
+            // txtBox01
             //
-            // txtBox34
-            // 
-            txtBox34.Name = "txtBox34";
-            txtBox34.Size = new Telerik.Reporting.Drawing.SizeU(Telerik.Reporting.Drawing.Unit.Cm(2.98979115486145D), Telerik.Reporting.Drawing.Unit.Cm(0.5D));
-            txtBox34.StyleName = "";
-            txtBox34.Value = "3.4";
+            txtBox01.Name = "txtBox01";
+            txtBox01.Size = new Telerik.Reporting.Drawing.SizeU(Telerik.Reporting.Drawing.Unit.Cm(3D), Telerik.Reporting.Drawing.Unit.Cm(0.5D));
+            txtBox01.StyleName = "";
+            txtBox01.Value = "0.1";
             //
-            // txtBox44
-            // 
-            txtBox44.Name = "txtBox44";
-            txtBox44.Size = new Telerik.Reporting.Drawing.SizeU(Telerik.Reporting.Drawing.Unit.Cm(2.98979115486145D), Telerik.Reporting.Drawing.Unit.Cm(0.5D));
-            txtBox44.StyleName = "";
-            txtBox44.Value = "4.4";
+            // txtBox10
             //
-            // txtBox54
-            // 
-            txtBox54.Name = "txtBox54";
-            txtBox54.Size = new Telerik.Reporting.Drawing.SizeU(Telerik.Reporting.Drawing.Unit.Cm(2.98979115486145D), Telerik.Reporting.Drawing.Unit.Cm(0.5D));
-            txtBox54.StyleName = "";
-            txtBox54.Value = "5.4";
+            txtBox10.Name = "txtBox10";
+            txtBox10.Size = new Telerik.Reporting.Drawing.SizeU(Telerik.Reporting.Drawing.Unit.Cm(3D), Telerik.Reporting.Drawing.Unit.Cm(0.5D));
+            txtBox10.StyleName = "";
+            txtBox10.Value = "1.0";
             //
-            // txtBox64
-            // 
-            txtBox64.Name = "txtBox64";
-            txtBox64.Size = new Telerik.Reporting.Drawing.SizeU(Telerik.Reporting.Drawing.Unit.Cm(2.98979115486145D), Telerik.Reporting.Drawing.Unit.Cm(0.5D));
-            txtBox64.StyleName = "";
-            txtBox64.Value = "6.4";
+            // txtBox11
+            //
+            txtBox11.Name = "txtBox11";
+            txtBox11.Size = new Telerik.Reporting.Drawing.SizeU(Telerik.Reporting.Drawing.Unit.Cm(3D), Telerik.Reporting.Drawing.Unit.Cm(0.5D));
+            txtBox11.StyleName = "";
+            txtBox11.Value = "1.1";
+        }
+
+        /// case 2:
+        /// | 0.0       |
+        /// -------------
+        /// | 1.0 | 1.1 |
+        /// 
+        private void CreateTable2x2MergeHeaderTest()
+        {
+            var table1 = new Telerik.Reporting.Table();
+            var colGroup1 = new Telerik.Reporting.TableGroup();
+            var colGroup2 = new Telerik.Reporting.TableGroup();
+            var colGroup3 = new Telerik.Reporting.TableGroup();
+            var detailGroup = new Telerik.Reporting.TableGroup();
+            var rowGroup1 = new Telerik.Reporting.TableGroup();
+
+            var txtBox00 = new Telerik.Reporting.TextBox();
+            var txtBox10 = new Telerik.Reporting.TextBox();
+            var txtBox11 = new Telerik.Reporting.TextBox();
+
+            this.detail.Items.AddRange(new Telerik.Reporting.ReportItemBase[] {
+            table1});
+
+            table1.Body.Columns.Add(new Telerik.Reporting.TableBodyColumn(Telerik.Reporting.Drawing.Unit.Cm(3D)));
+            table1.Body.Columns.Add(new Telerik.Reporting.TableBodyColumn(Telerik.Reporting.Drawing.Unit.Cm(3D)));
+            table1.Body.Rows.Add(new Telerik.Reporting.TableBodyRow(Telerik.Reporting.Drawing.Unit.Cm(0.5D)));
+
+            table1.Body.SetCellContent(0, 0, txtBox10);
+            table1.Body.SetCellContent(0, 1, txtBox11);
+
+            table1.Items.AddRange(new Telerik.Reporting.ReportItemBase[] {
+                txtBox00, txtBox10, txtBox11
+            });
+
+            colGroup2.Name = "colgroup-02";
+            colGroup3.Name = "colgroup-03";
+            colGroup1.ChildGroups.Add(colGroup2);
+            colGroup1.ChildGroups.Add(colGroup3);
+            colGroup1.Name = "colgroup-01";
+            colGroup1.ReportItem = txtBox00;
+
+            table1.ColumnGroups.Add(colGroup1);
+
+            rowGroup1.Name = "rowgroup-01";
+            detailGroup.ChildGroups.Add(rowGroup1);
+            detailGroup.Groupings.Add(new Telerik.Reporting.Grouping(null));
+            detailGroup.Name = "detailgroup";
+
+            table1.RowGroups.Add(detailGroup);
+
+            table1.Size = new Telerik.Reporting.Drawing.SizeU(Telerik.Reporting.Drawing.Unit.Cm(6D), Telerik.Reporting.Drawing.Unit.Cm(1D));
+            table1.Location = new Telerik.Reporting.Drawing.PointU(Telerik.Reporting.Drawing.Unit.Cm(1.5D), Telerik.Reporting.Drawing.Unit.Cm(1.5D));
+            table1.Name = "table1";
+
+            //
+            // txtBox00
+            //
+            txtBox00.Name = "txtBox00";
+            txtBox00.Size = new Telerik.Reporting.Drawing.SizeU(Telerik.Reporting.Drawing.Unit.Cm(6D), Telerik.Reporting.Drawing.Unit.Cm(0.5D));
+            txtBox00.StyleName = "";
+            txtBox00.Value = "0.0";
+            //
+            // txtBox10
+            //
+            txtBox10.Name = "txtBox10";
+            txtBox10.Size = new Telerik.Reporting.Drawing.SizeU(Telerik.Reporting.Drawing.Unit.Cm(3D), Telerik.Reporting.Drawing.Unit.Cm(0.5D));
+            txtBox10.StyleName = "";
+            txtBox10.Value = "1.0";
+            //
+            // txtBox11
+            //
+            txtBox11.Name = "txtBox11";
+            txtBox11.Size = new Telerik.Reporting.Drawing.SizeU(Telerik.Reporting.Drawing.Unit.Cm(3D), Telerik.Reporting.Drawing.Unit.Cm(0.5D));
+            txtBox11.StyleName = "";
+            txtBox11.Value = "1.1";
+        }
+
+        /// case 3:
+        /// | 0.0 | 0.1 |
+        /// -------------
+        /// | 1.0       |
+        /// 
+        private void CreateTable2x2MergeBodyCellTest()
+        {
+            var table1 = new Telerik.Reporting.Table();
+            var colGroup1 = new Telerik.Reporting.TableGroup();
+            var colGroup2 = new Telerik.Reporting.TableGroup();
+            var colGroup3 = new Telerik.Reporting.TableGroup();
+            var colGroup4 = new Telerik.Reporting.TableGroup();
+            var detailGroup = new Telerik.Reporting.TableGroup();
+            var rowGroup1 = new Telerik.Reporting.TableGroup();
+
+            var txtBox00 = new Telerik.Reporting.TextBox();
+            var txtBox01 = new Telerik.Reporting.TextBox();
+            var txtBox10 = new Telerik.Reporting.TextBox();
+
+            this.detail.Items.AddRange(new Telerik.Reporting.ReportItemBase[] {
+            table1});
+
+            table1.Body.Columns.Add(new Telerik.Reporting.TableBodyColumn(Telerik.Reporting.Drawing.Unit.Cm(3D)));
+            table1.Body.Columns.Add(new Telerik.Reporting.TableBodyColumn(Telerik.Reporting.Drawing.Unit.Cm(3D)));
+            table1.Body.Rows.Add(new Telerik.Reporting.TableBodyRow(Telerik.Reporting.Drawing.Unit.Cm(0.5D)));
+
+            table1.Body.SetCellContent(0, 0, txtBox10, 1, 2);
+
+            table1.Items.AddRange(new Telerik.Reporting.ReportItemBase[] {
+                txtBox00, txtBox01, txtBox10
+            });
+
+            colGroup2.Name = "colgroup-02";
+            colGroup1.ChildGroups.Add(colGroup2);
+            colGroup1.Name = "colgroup-01";
+            colGroup1.ReportItem = txtBox00;
+
+            colGroup4.Name = "colgroup-04";
+            colGroup3.ChildGroups.Add(colGroup4);
+            colGroup3.Name = "colgroup-03";
+            colGroup3.ReportItem = txtBox01;
+
+            table1.ColumnGroups.Add(colGroup1);
+            table1.ColumnGroups.Add(colGroup3);
+
+            rowGroup1.Name = "rowgroup-01";
+            detailGroup.ChildGroups.Add(rowGroup1);
+            detailGroup.Groupings.Add(new Telerik.Reporting.Grouping(null));
+            detailGroup.Name = "detailgroup";
+
+            table1.RowGroups.Add(detailGroup);
+
+            table1.Size = new Telerik.Reporting.Drawing.SizeU(Telerik.Reporting.Drawing.Unit.Cm(6D), Telerik.Reporting.Drawing.Unit.Cm(1D));
+            table1.Location = new Telerik.Reporting.Drawing.PointU(Telerik.Reporting.Drawing.Unit.Cm(1.5D), Telerik.Reporting.Drawing.Unit.Cm(1.5D));
+            table1.Name = "table1";
+
+            //
+            // txtBox00
+            //
+            txtBox00.Name = "txtBox00";
+            txtBox00.Size = new Telerik.Reporting.Drawing.SizeU(Telerik.Reporting.Drawing.Unit.Cm(3D), Telerik.Reporting.Drawing.Unit.Cm(0.5D));
+            txtBox00.StyleName = "";
+            txtBox00.Value = "0.0";
+            //
+            // txtBox01
+            //
+            txtBox01.Name = "txtBox01";
+            txtBox01.Size = new Telerik.Reporting.Drawing.SizeU(Telerik.Reporting.Drawing.Unit.Cm(3D), Telerik.Reporting.Drawing.Unit.Cm(0.5D));
+            txtBox01.StyleName = "";
+            txtBox01.Value = "0.1";
+            //
+            // txtBox10
+            //
+            txtBox10.Name = "txtBox10";
+            txtBox10.Size = new Telerik.Reporting.Drawing.SizeU(Telerik.Reporting.Drawing.Unit.Cm(6D), Telerik.Reporting.Drawing.Unit.Cm(0.5D));
+            txtBox10.StyleName = "";
+            txtBox10.Value = "1.0";
         }
     }
 }
